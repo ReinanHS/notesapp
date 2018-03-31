04d0c67d92bf76af06c44cc88274f6c123048a66
ReinanHS <reinangabriel1520@gmail.com>
2018-03-31 18:07:03 -0300

    Get the annotations

--- js/app.js
+++ js/app.js
@@ -62,6 +62,76 @@ function create_folder(folder, token){
     });
     return false;
 }
+function getAnnotations(){
+    console.log('Search the annotations...');
+    localStorage.setItem('annotations', '');
+    $.ajax({
+        url: 'https://api.dropboxapi.com/2/files/list_folder',
+        type: 'POST',
+        data: JSON.stringify({path: '/annotations'}),
+        datatype : "application/json",
+        contentType: "application/json",
+        headers: {
+        "Authorization": "Bearer "+getAccessToken()
+        },
+    }).done(function(annotations) {
+        console.log(annotations.entries.length+' files find with success!');
+        for (var i =  0; i < annotations.entries.length; i++) {
+            //console.log(annotations.entries[i]);
+            if(annotations.entries[i].path_display.slice(annotations.entries[i].path_display.indexOf("."),annotations.entries[i].path_display.length) == '.json'){
+                getTemporaryLink(annotations.entries[i].path_display);
+                var file = {
+                    id: annotations.entries[i].id,
+                    name: annotations.entries[i].name,
+                    path: annotations.entries[i].path_display,
+                    data: annotations.entries[i].server_modified,
+                    link: sessionStorage.getItem('annotationLink'),
+                }
+                addAnnotations(file, i);
+            }else{
+                console.log('The file "'+annotations.entries[i].path_display+'" not is a json');
+            }
+        }
+    })
+    .fail(function(error) {
+        console.log('Error on search the annotations!');
+        console.log(error);
+    });
+    return false; 
+}
+function getTemporaryLink(annotation){
+    console.log('Getting the link of annotation....');
+    $.ajax({
+        url: 'https://api.dropboxapi.com/2/files/get_temporary_link',
+        type: 'POST',
+        data: JSON.stringify({path: annotation}),
+        datatype : "application/json",
+        contentType: "application/json",
+        headers: {
+        "Authorization": "Bearer "+getAccessToken()
+        },
+    }).done(function(data) {
+        console.log('Get "'+annotation+'" link of annotations with success!');
+        sessionStorage.setItem('annotationLink', data.link);
+    })
+    .fail(function(error) {
+        console.log('Error on getting link of annotation!');
+        console.log(error);
+    });
+    return false;
+}
+function addAnnotations(annotation, index){
+    console.log('Adding the annotation "'+annotation.path+'" in LocalStorage!');
+    if(localStorage.getItem('annotations') == ''){
+        var annotations = [];
+        annotations[0] = annotation;
+        localStorage.setItem('annotations', JSON.stringify(annotations));
+    }else{
+        var annotations = JSON.parse(localStorage.getItem('annotations'));
+        annotations[index] = annotation;
+        localStorage.setItem('annotations', JSON.stringify(annotations));
+    }
+}
 function isAuthenticated(){
     return !!getAccessToken();
 }
