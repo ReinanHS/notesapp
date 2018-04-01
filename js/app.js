3777e5a9a8342d81f19ddbf0bd8b9180c0842fe8
ReinanHS <reinangabriel1520@gmail.com>
2018-04-01 16:29:00 -0300

    Create Annotation

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ function getTemporaryLink(annotation){
 }
 function addAnnotations(annotation, index){
     console.log('Adding the annotation "'+annotation.path+'" in LocalStorage!');
-    if(localStorage.getItem('annotations') == ''){
+    if(localStorage.getItem('annotations') === null || localStorage.getItem('annotations') == ''){
         var annotations = [];
         annotations[0] = annotation;
         localStorage.setItem('annotations', JSON.stringify(annotations));
@@ -130,6 +130,69 @@ function addAnnotations(annotation, index){
         var annotations = JSON.parse(localStorage.getItem('annotations'));
         annotations[index] = annotation;
         localStorage.setItem('annotations', JSON.stringify(annotations));
+    }
+}
+function addAnnotationsForUpalod(annotation, index){
+    console.log('Adding the annotation for upalod: "'+annotation.path+'" in LocalStorage!');
+    if(localStorage.getItem('annotationsForUpalod') === null || localStorage.getItem('annotationsForUpalod') == ''){
+        var annotations = [];
+        annotations[0] = annotation;
+        localStorage.setItem('annotationsForUpalod', JSON.stringify(annotations));
+    }else{
+        var annotations = JSON.parse(localStorage.getItem('annotationsForUpalod'));
+        annotations[index] = annotation;
+        localStorage.setItem('annotationsForUpalod', JSON.stringify(annotations));
+    }
+}
+function createAnnotation(annotation){
+    console.log('Creating annotation....');
+    $.ajax({
+        url: 'https://content.dropboxapi.com/2/files/alpha/upload',
+        type: 'POST',
+        data: JSON.stringify(annotation),
+        datatype : "application/octet-stream",
+        contentType: "application/octet-stream",
+        headers: {
+        "Authorization": "Bearer "+getAccessToken(),
+        "Dropbox-API-Arg": JSON.stringify({path: annotation.path}),
+        },
+    }).done(function(data) {
+        console.log('annotation create with success!');
+    })
+    .fail(function(error) {
+        console.log('Error on getting link of annotation!');
+        console.log(error);
+    });
+    return false;   
+}
+function updateAll(){
+    if(localStorage.getItem('annotationsForUpalod') === null || localStorage.getItem('annotationsForUpalod') == ''){
+        console.log('annotationsForUpalod is Null');
+    }
+    else{
+        var upload = JSON.parse(localStorage.getItem('annotationsForUpalod'));
+        for (var i =  0; i < upload.length; i++) {
+            createAnnotation(upload[i]);
+        }
+        localStorage.setItem('annotationsForUpalod', '');
+    }
+    getAnnotations();
+    return false;
+}
+function getFileName(){
+    console.log('Getting name of annotation');
+    var date = new Date();
+    var annotation = JSON.parse(sessionStorage.getItem('newAnnotation'));
+    if(annotation == null){
+        console.log('Name of annotation is '+('/annotations/'+date.getTime()+'.json'));
+        return ('/annotations/'+date.getTime()+'.json');
+    }else{
+        var name = annotation.title;
+        name = name.split(' ').join('');
+        name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        name = '/annotations/'+name+'-'+date.getTime()+'.json';
+        console.log('Name of annotation is '+name);
+        return name;
     }
 }
 function isAuthenticated(){
@@ -140,7 +203,74 @@ $(document).ready(function() {
         $('#login h1').html('Notesapp');
         $('#login .ui-content').html($('#loadAuthenticated').html());
         $('#loadAuthenticated').remove();
+        $('#btnCreateAnnotation').show();
     }else{
         $('#loginForDropbox').attr('href', 'https://www.dropbox.com/1/oauth2/authorize?response_type=token&client_id=88mpcrjr1g5q8fo&redirect_uri='+window.location);
     }
+
+    $('#formCreateAnnotation').submit(function(event) {
+        /* Act on the event */
+        var annotation = {
+            title: $('#un').val().replace(/<\/?[^>]+(>|$)/g, ""),
+            date: $('#date').val(),
+            type: $( "#formCreateAnnotation input[type='radio']:checked" ).val(),
+            path: getFileName(),
+        }
+        sessionStorage.setItem('newAnnotation', JSON.stringify(annotation));
+        if(annotation.type == 'choice-1'){
+            $('#editNormal').show();
+            $('#editLatex').hide();
+        }else if(annotation.type == 'choice-2'){
+            $('#editNormal').hide();
+            $('#editLatex').show();
+        }
+        $.mobile.changePage( "#editCard", { transition: "slideup", changeHash: false });
+        return false;
+    });
+
+    $('#trumbowyg-demo').trumbowyg({
+        lang: 'pt',
+        btns: [
+            ['viewHTML'],
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+        ],
+    });
+
+    $('#trumbowyg-latex').trumbowyg({
+        lang: 'pt',
+        btns: [
+            ['fullscreen'],
+        ],
+    });
+
+    $('#btnSaveAnnotation').click(function(event) {
+        var annotation = JSON.parse(sessionStorage.getItem('newAnnotation'));
+        if(annotation == null){
+            alert('Error ao salvar');
+        }else{
+            if(annotation.type == "choice-1"){
+                annotation.html = $('#trumbowyg-demo').trumbowyg('html');
+            }else if(annotation.type == "choice-2"){
+                annotation.latex = $('#trumbowyg-latex').trumbowyg('html');
+            }
+            sessionStorage.setItem('newAnnotation', JSON.stringify(annotation));
+
+            if(localStorage.getItem('annotations') === null || localStorage.getItem('annotations') == ''){
+                //addAnnotations(JSON.parse(sessionStorage.getItem('newAnnotation')), 0);
+                addAnnotationsForUpalod(JSON.parse(sessionStorage.getItem('newAnnotation')), 0);
+            }else{
+                var annotations = JSON.parse(localStorage.getItem('annotations'));
+                //addAnnotations(JSON.parse(sessionStorage.getItem('newAnnotation')), (annotations.length+1));
+                addAnnotationsForUpalod(JSON.parse(sessionStorage.getItem('newAnnotation')), (annotations.length+1));
+            }
+        }
+        $.mobile.changePage( "#login", { transition: "slideup", changeHash: false });
+        updateAll();
+    });
+
 });
